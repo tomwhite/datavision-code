ee5ecf8d76be901523c829995169193c7a566e46
Tom White <tom.e.white@gmail.com>
2020-01-13 14:54:45 +0000

    Add all cartogram visualizations

--- 03-wales-population-cartogram/wales_population_cartogram.R
+++ 03-wales-population-cartogram/wales_population_cartogram.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 library(rgdal)
+library(sf)
 library(cartogram)
+library(cowplot)
+library(plotly)
+library(htmlwidgets)
 
 # Population data for all Welsh local authorities
 
@@ -22,70 +26,37 @@ population <- load_population_data() %>%
 
 # Boundaries for all Welsh authorities
 # Source: http://lle.gov.wales/catalogue/item/LocalAuthorities
-
-# https://cengel.github.io/R-spatial/intro.html#loading-shape-files-into-r
+# Background: https://cengel.github.io/R-spatial/intro.html#loading-shape-files-into-r
 
 wales_spatial <- readOGR("data/HighWaterMark/")
-#plot(wales_spatial)
 
-# join pop data (need to preserve order, so don't use merge)
+# Join population data (need to preserve order, so don't use merge)
 library(plyr)
 wales_spatial@data <- join(wales_spatial@data, population)
 
-# Cartograms
-# https://www.r-graph-gallery.com/cartogram.html
+wales_sf <- st_as_sf(wales_spatial) %>%
+  st_simplify(preserveTopology=TRUE, dTolerance = 200) %>% # reduce data size
+  st_cast("MULTIPOLYGON") # this is needed for plotly to work
 
-#wales_cartogram <- cartogram_cont(wales_spatial, "population", itermax=5)
-#plot(wales_cartogram)
-
-# sf
-
-library(sf)
-#shapefile <- st_read("HighWaterMark/")
-#plot(shapefile)
-
-wales_sf = st_as_sf(wales_spatial)
-
-wales_sf <- wales_sf %>%
-  st_simplify(preserveTopology=TRUE, dTolerance = 200) %>%
-  st_cast("MULTIPOLYGON")
-plot(wales_sf)
-
-#str(wales_sf)
-#head(wales_sf)
-#tail(wales_sf)
-
-#powys = wales_sf[19,]
-#head(powys)
-#plot(powys)
+# Create a cartogram (https://www.r-graph-gallery.com/cartogram.html)
 
 wales_cartogram_sf <- cartogram_cont(wales_sf, "population", itermax = 15, prepare = "none") %>%
   st_cast("MULTIPOLYGON")
-#plot(wales_cartogram_sf["population"])
 
-# TODO: add LA labels
-# Using plotly fails due to https://github.com/ropensci/plotly/issues/1659. See also https://community.plot.ly/t/how-to-make-tooltips-show-in-region-rather-than-on-border-map-using-ggplotly/13507
+# Plot the regular map and the cartogram side by side
+# Use cowplot https://wilkelab.org/cowplot/articles/plot_grid.html
 
-p1 <- ggplot(wales_sf["population"], aes(fill = population/1000, text = wales_sf$name_en)) +
-  theme_void() +
-  geom_sf() +
-  # geom_sf_text(label = wales_sf$name_en, size = 3) + # TODO: specify labels directly (only 22), see https://stackoverflow.com/questions/7611169/intelligent-point-label-placement-in-r
-  scale_fill_distiller(palette = "Greens", direction = 1) +
-  labs(fill = "Population (thousands)")
-
-#library(plotly)
-#ggplotly(p1) %>% style(hoveron = "fill")
-
-p2 <- ggplot(wales_cartogram_sf["population"], aes(fill = population/1000, text = wales_sf$name_en)) +
+p1 <- ggplot(wales_sf["population"], aes(fill = population/1000, text = paste0(wales_sf$name_en, ": ", round(population/1000, 0)))) +
   theme_void() +
   geom_sf() +
   scale_fill_distiller(palette = "Greens", direction = 1) +
   labs(fill = "Population (thousands)")
 
-#ggplotly(p2) %>% style(hoveron = "fill")
-
-# https://wilkelab.org/cowplot/articles/plot_grid.html
-library("cowplot")
+p2 <- ggplot(wales_cartogram_sf["population"], aes(fill = population/1000, text = paste0(wales_sf$name_en, ": ", round(population/1000, 0)))) +
+  theme_void() +
+  geom_sf() +
+  scale_fill_distiller(palette = "Greens", direction = 1) +
+  labs(fill = "Population (thousands)")
 
 legend <- get_legend(
   # create some space to the left of the legend
@@ -102,40 +73,49 @@ prow <- plot_grid(
   p1 + theme(legend.position = "none"),
   p2 + theme(legend.position = "none")
 )
-#plot_grid(prow, legend, rel_widths = c(2, .4))
-plot_grid(prow, legend_b, ncol = 1, rel_heights = c(1, .1))
 
-#subplot(ggplotly(p1) %>% style(hoveron = "fill"), ggplotly(p2) %>% style(hoveron = "fill"))
+title <- ggdraw() + 
+  draw_label(
+    "Population by local authority in Wales (area adjusted), 2018",
+    fontface = 'bold',
+    x = 0.5
+  )
 
-#install.packages("transformr")
+plot_grid(title, prow, legend_b, ncol = 1, rel_heights = c(.1, 0.5, .1))
+
+ggsave("03-wales-population-cartogram.png", width = 9, height = 6, dpi = "retina")
+
+# An interactive version (with tooltips for local authorities) using plotly
+
+interactive <- subplot(ggplotly(p1, tooltip = c('text')) %>% style(hoveron = "fill"), ggplotly(p2, tooltip = c('text')) %>% style(hoveron = "fill"))
+saveWidget(interactive, "03-wales-population-cartogram-interactive.html")
+
+# Finally, an animated version, which transitions from the regular map to the area adjusted map.
+# I prefer the side-by-side version since the animation isn't very smooth (lots of gaps appear for some reason),
+# and it's hard to compare since it keeps moving!
+
+#install.packages("animation")
+#install.packages("/Users/tom/Downloads/gganimate-0.1.1", repos = NULL, type="source")
+
+library(gganimate)
 library(transformr)
 
 morph <- tween_sf(wales_sf, wales_cartogram_sf,
                   ease = 'linear',
                   nframes = 20)
 
+# Look at the frames statically
 ggplot(morph) + 
   geom_sf(aes(geometry = geometry, fill = population), colour = 'white' , size = .1) + 
   facet_wrap(~.frame, labeller = label_both, ncol = 3) + 
   coord_sf(datum = NULL) + 
   theme_void()
 
-# TODO: just show side by side view - animation isn't very smooth (lots of gaps appear) and is hard to look at
-
-#remove.packages("gganimate")
-#install.packages('gifski')
-#install.packages('png')
-
-#install.packages("animation")
-#install.packages("/Users/tom/Downloads/gganimate-0.1.1", repos = NULL, type="source")
-
-library(gganimate)
-
-
+# Create an animated gif
 p <- ggplot(morph) + 
   geom_sf(aes(geometry = geometry, fill = population, frame = .frame), colour = 'white' , size = .1) + 
   coord_sf() + 
   theme_void()
   
 animation::ani.options(interval = 1/20)
-gganimate(p, "animation.gif")
+gganimate(p, "03-wales-population-cartogram-animation.gif")
